5737d06511bc4f37460eb8743299cee10e1a42b5
zachdressing <zed_dressel@hotmail.com>
2024-02-11 16:31:34 -0800

    Added API function to all pages

--- src/components/M8BComponent.js
+++ src/components/M8BComponent.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState, React } from 'react';
 import NavbarComponent from './NavbarComponent'
 import { Flowbite } from 'flowbite-react';
+import { ballAPI } from '../DataService';
 
-function M8BComponent() {
+function RiAComponent() {
+    const [num1, setNum1] = useState(0);
+    const [output, setOut] = useState('');
+
+    const getOutput = async () => {
+        const response = await ballAPI(num1);
+        setOut(response);
+    }
     return (
         <>
             <div className='bgStuff'>
@@ -11,17 +19,14 @@ function M8BComponent() {
                     <div className="bg-grey h-fit w-fot flex justify-center p-5">
                         <div className="w-full text-[48px] text-center text-white p-4">
                             <form className="flex flex-col items-center">
-                                <h1>
-                                    <label for="input1" className="p-5">Input:</label>
-                                    <input type="text" id="input1" name="input1" placeholder="your question here" className='text-black' />
-                                </h1>
+                                <h1><label for="input1" className="p-5">Input:</label><input type="text" onChange={(e) => { setNum1(e.target.value) }} id="input1" name="input1" placeholder="your question" className='text-black' /></h1>
                             </form>
-                            <h1 id="outputLine" className='flex'>Output: <br /> <span id="output"></span></h1>
+                            <h1 id="outputLine" className='flex'>Output: <br /> <span id="output">{output}</span></h1>
                         </div>
                     </div>
                 </div>
                     <div className="buttonDiv flex justify-center items-center pt-32">
-                        <button id="btn" className="border-2 border-gray-900 bg-gray-100 w-48 h-20 hover:brightness-50">
+                        <button id="btn" onClick={() => getOutput()} className="border-2 border-gray-900 bg-gray-100 w-48 h-20 hover:brightness-50">
                             <i className="text-gray-900 p-2 text-2xl text-5xl">
                                 enter
                             </i>
@@ -32,4 +37,4 @@ function M8BComponent() {
             )
 }
 
-            export default M8BComponent;
+            export default RiAComponent;
